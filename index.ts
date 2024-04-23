d935fa018f7262a79cca397fc590abb2d4f2e171
M Daniyal Sahito <160993870+M-Daniyal-Sahito@users.noreply.github.com>
2024-04-23 15:41:36 +0500

    This is my calculator which i created in ramzan.

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { massage: "First Number", type: "number", name: "firstNumber" },
+  { massage: "Second Number", type: "number", name: "secondNumber" },
+  {
+    massage: "Select Operation to perform Operation",
+    type: "list",
+    choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+    name: "operators",
+  },
+]);
+
+if (answer.operators === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operators === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operators === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else if (answer.operators === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else {
+  console.log("invailid number");
+}
